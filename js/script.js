e8c8ff88178ef31d45131cdec8b0cd0e44054a46
Abhi Khatri <abhikhatri391@gmail.com>
2016-08-28 00:43:55 +0530

    Added song progress bar

--- js/script.js
+++ js/script.js
@@ -8,8 +8,17 @@ var TRACK = "/tracks/278556380";
 
 $(document).ready(function(){
 
+  var progressTracker;
+  var trackDuration;
+  
+  var updatePosition = function(currentTrack){
+    var currentPosition = currentTrack.currentTime();
+    console.log('Current: '+ currentPosition);
+    $("#sc-progress-fill").css('width', ((currentPosition / trackDuration) * 100) + '%');
+  };
+
   SC.get(TRACK).then(function(track){
-    console.log(track);
+    trackDuration = track.duration;
     var artwork = track.artwork_url;
     artwork = artwork.replace('-large', '-crop');
     $('#sc-artwork').prepend('<img src="'+ artwork +'" />');
@@ -21,17 +30,32 @@ $(document).ready(function(){
   });
 
 
-
   SC.stream(TRACK).then(function(player){
+
     $('#sc-artwork').on('click', '#mediaBtn.paused', function(){
       $('#mediaBtn').removeClass('paused').addClass('playing');
       player.play();
+      progressTracker = setInterval( function(){ updatePosition(player); }, 1000);
     });
 
     $('#sc-artwork').on('click', '#mediaBtn.playing', function(){
       $('#mediaBtn').removeClass('playing').addClass('paused');
       player.pause();
+      clearInterval(progressTracker);
     });
+
+    player.on('finish', function(){
+      clearInterval(progressTracker);
+      $('#mediaBtn').removeClass('playing').addClass('paused');
+    });
+
+    $('.sc-progress-bar').on('click', function(event){
+      var x = event.pageX - $(this).offset().left;
+      var width = $(this).width();
+      player.seek((x / width) * trackDuration);
+      updatePosition(player);
+    });
+
   });
 
 });
